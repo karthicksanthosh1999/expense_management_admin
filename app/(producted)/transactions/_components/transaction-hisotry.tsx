e73f53e84584eaebd75e127112a498030968f916
karthicksanthosh <karthick.santhosh1999@gmail.com>
2026-03-30 23:15:01 +0530

    Wokring on the transaction model

--- app/(producted)/transactions/_components/transaction-hisotry.tsx
+++ app/(producted)/transactions/_components/transaction-hisotry.tsx
@@ -22,69 +22,76 @@ type props = {
 };
 
 const TransactionHistory = ({ transactionData, loading }: props) => {
+  const [transactionModelOpen, setTransactionModelOpen] = useState(false);
+
   const [transaction, setTransaction] = useState<ITransaction | null>(null);
   const handleModelClose = () => {
+    setTransactionModelOpen(false);
     setTransaction(null);
+  };
+
+  const handleModelOpen = () => {
+    setTransactionModelOpen(true);
   };
 
   return (
     <>
-      <Dialog>
-        <DialogTrigger className={"w-full"}>
-          <Card className="w-full h-250">
-            <CardContent>
-              <CardHeader className="flex items-center justify-between">
-                <h1 className="text-2xl font-semibold text-color">
-                  Transaction History
-                </h1>
-                <p className="text-gray-400">Showing 1-8 of 16 transactions</p>
-              </CardHeader>
-              <div className="relative">
-                <Separator className={"my-5"} />
-                {loading && (
-                  <div className="absolute top-1 left-0 w-full">
-                    <DataLoader />
-                  </div>
-                )}
-              </div>
-              <section className="space-y-3">
-                {transactionData &&
-                  transactionData.map((item, idx) => (
-                    <div
-                      onClick={() => setTransaction(item)}
-                      key={idx}
-                      className="flex items-center justify-between border border-blue-900 hover:border-primary p-3 cursor-pointer rounded-xl transaction ease-in-out duration-500 hover:translate-y-1">
-                      <div className="flex items-center gap-5">
-                        <div className="flex flex-col items-start">
-                          <h1 className="text-color text-lg font-semibold tracking-wider">
-                            {item.message}
-                          </h1>
-                          <p className="text-gray-400 tracking-wider">
-                            {format(item.transactionDate, "dd-mm-yyyy")}
-                          </p>
-                        </div>
-                      </div>
-                      <div>
-                        <h1
-                          className="text-lg font-semibold"
-                          style={{
-                            color:
-                              item.transactionType === "EXPENSE"
-                                ? "#fb2c36"
-                                : "oklch(72.3% 0.219 149.579)",
-                          }}>
-                          ${item.amount}
+      <Dialog
+        open={transactionModelOpen}
+        onOpenChange={setTransactionModelOpen}>
+        <Card onClick={handleModelOpen} className="w-full h-250">
+          <CardContent>
+            <CardHeader className="flex items-center justify-between">
+              <h1 className="text-2xl font-semibold text-color">
+                Transaction History
+              </h1>
+              <p className="text-gray-400">Showing 1-8 of 16 transactions</p>
+            </CardHeader>
+            <div className="relative">
+              <Separator className={"my-5"} />
+              {loading && (
+                <div className="absolute top-1 left-0 w-full">
+                  <DataLoader />
+                </div>
+              )}
+            </div>
+            <section className="space-y-3">
+              {transactionData &&
+                transactionData.map((item, idx) => (
+                  <div
+                    onClick={() => setTransaction(item)}
+                    key={idx}
+                    className="flex items-center justify-between border border-blue-900 hover:border-primary p-3 cursor-pointer rounded-xl transaction ease-in-out duration-500 hover:translate-y-1">
+                    <div className="flex items-center gap-5">
+                      <div className="flex flex-col items-start">
+                        <h1 className="text-color text-lg font-semibold tracking-wider">
+                          {item.message}
                         </h1>
-                        <p className="text-color text-lg font-semibold">
-                          <StatusChip status={item.transactionType} />
+                        <p className="text-gray-400 tracking-wider">
+                          {format(item.transactionDate, "dd-mm-yyyy")}
                         </p>
                       </div>
                     </div>
-                  ))}
-              </section>
-            </CardContent>
-          </Card>
-        </DialogTrigger>
+                    <div>
+                      <h1
+                        className="text-lg font-semibold"
+                        style={{
+                          color:
+                            item.transactionType === "EXPENSE"
+                              ? "#fb2c36"
+                              : "oklch(72.3% 0.219 149.579)",
+                        }}>
+                        ${item.amount}
+                      </h1>
+                      <p className="text-color text-lg font-semibold">
+                        <StatusChip status={item.transactionType} />
+                      </p>
+                    </div>
+                  </div>
+                ))}
+            </section>
+          </CardContent>
+        </Card>
         <DialogContent className="sm:max-w-lg bg-card">
           <DialogHeader>
             <div className="flex justify-between items-center">
@@ -99,12 +106,37 @@ const TransactionHistory = ({ transactionData, loading }: props) => {
             <div className="flex items-center justify-center flex-col gap-2">
               <SmartphoneIcon size={30} />
               <h1 className="font-semibold text-4xl">₹{transaction?.amount}</h1>
-              <p className="text-base font-normal">{transaction?.message}</p>
+              <p className="text-base font-normal pb-5">
+                {transaction?.message}
+              </p>
+            </div>
+
+            <div>
+              <Card>
+                <CardContent className="space-y-3">
+                  <div className="flex items-center justify-between text-sm">
+                    <p>Transaction Type:</p>
+                    <p>
+                      {transaction?.transactionType && (
+                        <StatusChip status={transaction.transactionType} />
+                      )}
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-between text-sm">
+                    <p>Transaction Category:</p>
+                    <p>{transaction?.category}</p>
+                  </div>
+                  <div className="flex items-center justify-between text-sm">
+                    <p>Transaction Date:</p>
+                    <div>{new Date(transaction?.transactionDate)}</div>
+                  </div>
+                </CardContent>
+              </Card>
             </div>
 
             <div className="space-x-5 flex items-center justify-center">
               <Button variant={"outline"} className="h-10 w-30">
-                Close
+                Download Recept
               </Button>
               <Button variant={"default"} className="h-10 w-30 text-white">
                 Close
